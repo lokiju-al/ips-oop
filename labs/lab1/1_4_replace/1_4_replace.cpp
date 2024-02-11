30fcc011976915c5aac831a07ac42d643f2e241e
alexander <platov0102@gmail.com>
2024-02-12 00:06:51 +0300

    1_4 copyfile 43min

--- labs/lab1/1_4_replace/1_4_replace.cpp
+++ labs/lab1/1_4_replace/1_4_replace.cpp
@@ -6,18 +6,34 @@
 
 int main(int argc, char* argv[])
 {
+	if (argc != 3)
+	{
+		std::cout << "Invalid arguments count\n";
+		std::cout << "Usage: CopyFile.exe <input>";
+		return 1;
+	}
+
 	std::ifstream input;
 	input.open(argv[1]);
+	if (!input.is_open())
+	{
+		std::cout << "Failed to open '" << argv[1] << "' for reading\n";
+		return 1;
+	}
+
+	std::ofstream output;
+	output.open(argv[2]);
+	if (!output.is_open())
+	{
+		std::cout << "Failed to open '" << argv[2] << "' for writing\n";
+		return 1;
+	}
+
 	char ch;
 	while (input.get(ch))
 	{
-		std::cout.put(ch);
+		output.put(ch);
 	}
 
-	/* std::cout << argc << "\n";
-	for (int i = 0; i < argc; ++i)
-	{
-		std::cout << argv[i] << "\n";
-	}*/
 	return 0;
 }
