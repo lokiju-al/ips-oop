4f24a2ac6636bd6257da3c1ed4108b6caf2c29d6
alexander <platov0102@gmail.com>
2024-02-25 03:14:04 +0300

    2_5_flipbyte прикрутил тесты. Готово к сдаче

---
+++ tasks/01/2_5_flipbyte/flipbyte/Tests.bat
@@ -0,0 +1,66 @@
+@echo off
+
+REM Путь к тестируемой программе передается через первый аргумент командной строки
+SET MyProgram="%~1"
+
+REM Защита от запуска без аргумента, задающего путь к программе
+if %MyProgram%=="" (
+	echo Please specify path to program
+	exit /B 1
+)
+
+REM ------------------------------- НЕГАТИВНЫЕ КЕЙСЫ -------------------------------
+echo -------------
+REM Запуск без параметров
+echo Input:
+%MyProgram% && goto err
+echo Test 1 passed
+echo -------------
+REM Запуск с лишним параметром
+echo Input: 123 йцу
+%MyProgram% 123 йцу && goto err
+echo Test 2 passed
+echo -------------
+REM Запуск с параметром не числом
+echo Input: йцу
+%MyProgram% йцу && goto err
+echo Test 3 passed
+echo -------------
+REM Запуск с параметром меньше нуля
+echo Input: -6
+%MyProgram% -6 && goto err
+echo Test 4 passed
+echo -------------
+REM Запуск с параметром не входящим в область допустимых значений
+echo Input: 256
+%MyProgram% 256 && goto err
+echo Test 5 passed
+echo -------------
+REM ------------------------------- ПОЗИТИВНЫЕ КЕЙСЫ -------------------------------
+REM Переворот битов в пограничнвх значениях
+echo Input: 0
+%MyProgram% 0 || goto err
+echo Test 6 passed
+echo -------------
+echo Input: 255
+%MyProgram% 255 || goto err
+echo Test 7 passed
+echo -------------
+echo Input: 1
+%MyProgram% 1 || goto err
+echo Test 8 passed
+echo -------------
+REM Переворот битов в симметричных битах
+echo Input: 36
+%MyProgram% 36 || goto err
+echo Test 9 passed
+echo -------------
+REM -------------------------------------------------------------------------------
+REM Тесты прошли успешно
+echo All tests passed successfuly
+exit /B 0
+
+REM Сюда будем попадать в случае ошибки
+:err
+echo Test failed
+exit /B 1
