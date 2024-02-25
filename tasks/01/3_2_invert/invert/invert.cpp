8d68944976143438b64030a95630235ed67120dd
Alexander Platov <platov0102@gmail.com>
2024-02-25 18:21:01 +0300

    3_2_invert вывод на экран матрицы из файла

--- tasks/01/3_2_invert/invert/invert.cpp
+++ tasks/01/3_2_invert/invert/invert.cpp
@@ -1,20 +1,69 @@
-// invert.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+#include <fstream>
 #include <iostream>
+#include <optional>
+#include <string>
 
-int main()
+struct Args
 {
-    std::cout << "Hello World!\n";
+	std::string inputFileName;
+};
+
+std::optional<Args> ParseArgs(int argc, char* argv[])
+{
+	if (argc != 2)
+	{
+		std::cout << "Invalid arguments count\n";
+		std::cout << "Usage: invert.exe <matrix file>\n";
+		return std::nullopt;
+	}
+	Args args;
+	args.inputFileName = argv[1];
+	return args;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void ReadArray(std::istream& input, int arr[3][3])
+{
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			input >> arr[i][j];
+		}
+	}
+	std::cout << "inputArray\n";
+	std::cout << arr[0][0] << " " << arr[0][1] << " " << arr[0][2] << "\n";
+	std::cout << arr[1][0] << " " << arr[1][1] << " " << arr[1][2] << "\n";
+	std::cout << arr[2][0] << " " << arr[2][1] << " " << arr[2][2] << "\n";
+}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+int main(int argc, char* argv[])
+{
+	auto args = ParseArgs(argc, argv);
+	// Проверка правильности аргументов командной строки
+	if (!args)
+	{
+		return 1;
+	}
+
+	int inputArray[3][3];
+
+	// Открываем входной файл для чтения
+	std::ifstream input;
+	input.open(args->inputFileName);
+	if (!input.is_open())
+	{
+		std::cout << "Failed to open '" << args->inputFileName << "' for reading\n";
+		return 1;
+	}
+
+	// Считываем числа из входного файла в массив
+	ReadArray(input, inputArray);
+
+	if (input.bad())
+	{
+		std::cout << "Failed to read data from input file";
+		return 1;
+	}
+
+	return 0;
+}
