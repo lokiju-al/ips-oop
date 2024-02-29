3c00d07cb41832abc6068d9e710ebe1c6d774406
alexander <platov0102@gmail.com>
2024-03-01 01:38:10 +0300

    3_2_invert Готово к сдаче

---
+++ tasks/01/3_2_invert/invert/Tests.bat
@@ -0,0 +1,59 @@
+﻿@echo off
+
+REM Путь к тестируемой программе передается через первый аргумент командной строки
+SET MyProgram="%~1"
+
+REM Защита от запуска без аргумента, задающего путь к программе
+if %MyProgram%=="" (
+	echo Please specify path to program
+	exit /B 1
+)
+
+REM ------------------------------- НЕГАТИВНЫЕ КЕЙСЫ -------------------------------
+echo -------------
+REM Запуск без параметров
+%MyProgram% && goto err
+echo Test 1 passed
+echo -------------
+REM Запуск с лишним параметром
+%MyProgram% MatrixFile.txt 123 && goto err
+echo Test 2 passed
+echo -------------
+REM Запуск с несуществующим файлом в параметрах
+%MyProgram% MissingFile.txt && goto err
+echo Test 3 passed
+echo -------------
+REM ------------------------------- ПОЗИТИВНЫЕ КЕЙСЫ -------------------------------
+REM Запуск с файлом в параметрах
+%MyProgram% Test1.txt || goto err
+echo Test 4 passed
+echo -------------
+REM Запуск с файлом в параметрах
+%MyProgram% Test2.txt || goto err
+echo Test 5 passed
+echo -------------
+REM Запуск с файлом в параметрах
+%MyProgram% Test3.txt || goto err
+echo Test 6 passed
+echo -------------
+REM Запуск с файлом в параметрах
+%MyProgram% Test4.txt || goto err
+echo Test 7 passed
+echo -------------
+REM Запуск с файлом в параметрах
+%MyProgram% Test5.txt || goto err
+echo Test 8 passed
+echo -------------
+REM Запуск с файлом в параметрах
+%MyProgram% Test6.txt || goto err
+echo Test 9 passed
+echo -------------
+REM -------------------------------------------------------------------------------
+REM Тесты прошли успешно
+echo All tests passed successfuly
+exit /B 0
+
+REM Сюда будем попадать в случае ошибки
+:err
+echo Test failed
+exit /B 1
--- tasks/01/3_2_invert/invert/invert.cpp
+++ tasks/01/3_2_invert/invert/invert.cpp
@@ -2,9 +2,10 @@
 #include <iostream>
 #include <optional>
 #include <string>
+#include <iomanip>
 
-using Matrix2x2 = int[2][2];
-using Matrix3x3 = int[3][3];
+using Matrix2x2 = float[2][2];
+using Matrix3x3 = float[3][3];
 
 struct Args
 {
@@ -27,46 +28,62 @@ std::optional<Args> ParseArgs(int argc, char* argv[])
 // Считываем числа из входного файла в массив
 void ReadArray(std::istream& input, Matrix3x3 arr)
 {
+	std::cout << "Input matrix:\n";
 	for (int i = 0; i < 3; i++)
 	{
 		for (int j = 0; j < 3; j++)
 		{
 			input >> arr[i][j];
+			std::cout << arr[i][j] << " ";
 		}
+		std::cout << "\n";
 	}
-	std::cout << "inputArray\n";
-	std::cout << arr[0][0] << " " << arr[0][1] << " " << arr[0][2] << "\n";
-	std::cout << arr[1][0] << " " << arr[1][1] << " " << arr[1][2] << "\n";
-	std::cout << arr[2][0] << " " << arr[2][1] << " " << arr[2][2] << "\n";
 }
 
-int DeterminantOfArray(Matrix3x3 arr, int n)
+float DeterminantOfArray(Matrix3x3 arr)
 {
-	int det = 0;
-	if (n == 2)
-		det = arr[0][0] * arr[1][1] - arr[1][0] * arr[0][1];
-	else if (n == 3)
-	{
-		det = arr[0][0] * arr[1][1] * arr[2][2] + arr[2][0] * arr[1][2] * arr[0][1] + arr[0][2] * arr[1][0] * arr[2][1] 
-			- (arr[0][2] * arr[1][1] * arr[2][0] + arr[0][1] * arr[1][0] * arr[2][2] + arr[0][0] * arr[1][2] * arr[2][1]);
-	}
-	else
-	{
-		return 0;
-	}
-	return det;
+	return arr[0][0] * arr[1][1] * arr[2][2] + arr[2][0] * arr[1][2] * arr[0][1] 
+			+ arr[0][2] * arr[1][0] * arr[2][1] - arr[0][2] * arr[1][1] * arr[2][0] 
+			- arr[0][1] * arr[1][0] * arr[2][2] - arr[0][0] * arr[1][2] * arr[2][1];
+}
+
+void AdditionArray(Matrix3x3 inp, Matrix3x3 add)
+{
+	add[0][0] = (inp[1][1] * inp[2][2] - inp[2][1] * inp[1][2]);
+	add[0][1] = (inp[1][0] * inp[2][2] - inp[2][0] * inp[1][2]) * (-1);
+	add[0][2] = (inp[1][0] * inp[2][1] - inp[2][0] * inp[1][1]);
+	add[1][0] = (inp[0][1] * inp[2][2] - inp[2][1] * inp[0][2]) * (-1);
+	add[1][1] = (inp[0][0] * inp[2][2] - inp[2][0] * inp[0][2]);
+	add[1][2] = (inp[0][0] * inp[2][1] - inp[2][0] * inp[0][1]) * (-1);
+	add[2][0] = (inp[0][1] * inp[1][2] - inp[1][1] * inp[0][2]);
+	add[2][1] = (inp[0][0] * inp[1][2] - inp[1][0] * inp[0][2]) * (-1);
+	add[2][2] = (inp[0][0] * inp[1][1] - inp[1][0] * inp[0][1]);
+}
+
+void AttachedArray(Matrix3x3 inp, Matrix3x3 add)
+{
+	inp[0][0] = add[0][0];
+	inp[0][1] = add[1][0];
+	inp[0][2] = add[2][0];
+	inp[1][0] = add[0][1];
+	inp[1][1] = add[1][1];
+	inp[1][2] = add[2][1];
+	inp[2][0] = add[0][2];
+	inp[2][1] = add[1][2];
+	inp[2][2] = add[2][2];
 }
 
 int main(int argc, char* argv[])
 {
 	auto args = ParseArgs(argc, argv);
+
 	// Проверка правильности аргументов командной строки
 	if (!args)
 	{
 		return 1;
 	}
 
-	Matrix3x3 inputArray;
+	Matrix3x3 inputArray, additionArray;
 
 	// Открываем входной файл для чтения
 	std::ifstream input;
@@ -78,23 +95,37 @@ int main(int argc, char* argv[])
 	}
 
 	ReadArray(input, inputArray);
-	std::cout << "DeterminantOfArray: " << DeterminantOfArray(inputArray, 3);
-
 	if (input.bad())
 	{
-		std::cout << "Failed to read data from input file";
+		std::cout << "Failed to read data from input file\n";
 		return 1;
 	}
 
+	float determinantOfInputArray = DeterminantOfArray(inputArray);
+	std::cout << "Determinant of input matrix: " << determinantOfInputArray << "\n";
+	if (determinantOfInputArray == 0)
+	{
+		std::cout << "The matrix is singular and there is no inverse for it\n";
+		return 0;
+	}
 
+	AdditionArray(inputArray, additionArray);
 
+	AttachedArray(inputArray, additionArray);
 
-
-
-
-
-
-
+	std::cout << "Inverse matrix:\n";
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			if (inputArray[i][j] == 0)
+			{
+				inputArray[i][j] = 0;
+			}
+			std::cout << std::setprecision(3) << (inputArray[i][j] / determinantOfInputArray) << " ";
+		}
+		std::cout << "\n";
+	}
 
 	return 0;
 }
