49d76473006f75f5308e153674172b5f0a3c48b0
alexander <platov0102@gmail.com>
2024-03-14 23:20:01 +0300

    2_1 vector sample

--- tasks/02/1_vector/vector/src/main/kotlin/Main.kt
+++ tasks/02/1_vector/vector/src/main/kotlin/Main.kt
@@ -1,5 +1,13 @@
-fun main() {
+fun readIntArrayFromString(input: String): IntArray {
+    val numbers = input.split(" ").map { it.toInt() }
+    return numbers.toIntArray()
+}
 
+
+fun main() {
+    val input = "10 20 30 40 50"
+    val array = readIntArrayFromString(input)
+    println("Считанный массив чисел: ${array.joinToString()}")
     print("Input floating point numbers separated by spaces: ")
     val name = readlnOrNull()
 
