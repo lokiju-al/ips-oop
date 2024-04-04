e04f6cab8a48f28cfe83c7598af37f560789fbd1
alexander <platov0102@gmail.com>
2024-04-04 06:57:50 +0300

    3_1_Car отрефакторил несколько методов

--- tasks/03/1_car/car/src/main/kotlin/Main.kt
+++ tasks/03/1_car/car/src/main/kotlin/Main.kt
@@ -1,13 +1,3 @@
-enum class Gear(val range: IntRange) {
-    REVERSE(0..20),
-    NEUTRAL(Int.MIN_VALUE..Int.MAX_VALUE),
-    FIRST(0..30),
-    SECOND(20..50),
-    THIRD(30..60),
-    FOURTH(40..90),
-    FIFTH(50..150)
-}
-
 enum class Direction {
     FORWARD,
     BACKWARD,
@@ -16,45 +6,48 @@ enum class Direction {
 
 class Car {
     private var engineOn: Boolean = false
-    private var currentGear: Gear = Gear.NEUTRAL
+    private var currentGear: Int = 0
     private var currentSpeed: Int = 0
 
-    fun isEngineOn(): Boolean {
+    private fun GetSpeedRangeForGear(gear: Int): IntRange {
+        return when (gear) {
+            -1 -> 0..20
+            0 -> 0..Int.MAX_VALUE
+            1 -> 0..30
+            2 -> 20..50
+            3 -> 30..60
+            4 -> 40..90
+            5 -> 50..150
+            else -> 0..0
+        }
+    }
+
+    fun IsTurnedOn(): Boolean {
         return engineOn
     }
 
-    fun getDirection(): Direction {
+    fun GetDirection(): Direction {
         return when {
-            currentGear == Gear.REVERSE -> Direction.BACKWARD
             currentSpeed == 0 -> Direction.STANDING_STILL
-            currentGear == Gear.NEUTRAL -> Direction.STANDING_STILL
-            currentSpeed > 0 -> Direction.FORWARD
-            else -> Direction.STANDING_STILL
+            currentSpeed > 0 && currentGear == -1 -> Direction.BACKWARD
+            else -> Direction.FORWARD
         }
     }
 
-    fun getSpeed(): Int {
+    fun GetSpeed(): Int {
         return currentSpeed
     }
 
-    fun getGear(): Int {
-        return when (currentGear) {
-            Gear.REVERSE -> -1
-            Gear.NEUTRAL -> 0
-            else -> currentGear.ordinal
-        }
+    fun GetGear(): Int {
+        return currentGear
     }
 
-    fun turnOnEngine(): Boolean {
-        if (!engineOn) {
-            engineOn = true
-            currentGear = Gear.NEUTRAL
-            return true
-        }
-        return false
+    fun TurnOnEngine(): Boolean {
+        engineOn = true
+        return true
     }
 
-    fun turnOffEngine(): Boolean {
+    fun TurnOffEngine(): Boolean {
         if (engineOn && currentSpeed == 0 && currentGear == Gear.NEUTRAL) {
             engineOn = false
             return true
@@ -62,9 +55,9 @@ class Car {
         return false
     }
 
-    fun setGear(gear: Int): Boolean {
+    fun SetGear(gear: Int): Boolean {
         val newGear = when (gear) {
-            in Gear.values().indices -> Gear.values()[gear]
+            in Gear.entries.toTypedArray().indices -> Gear.entries[gear]
             else -> null
         }
         if (newGear != null && (newGear == Gear.NEUTRAL || (engineOn && currentSpeed in newGear.range))) {
@@ -74,7 +67,7 @@ class Car {
         return false
     }
 
-    fun setSpeed(speed: Int): Boolean {
+    fun SetSpeed(speed: Int): Boolean {
         return if (engineOn && currentGear != Gear.NEUTRAL && speed in currentGear.range) {
             currentSpeed = speed
             true
@@ -86,41 +79,52 @@ fun main() {
     val car = Car()
 
     while (true) {
-        val input = readLine()?.trim() ?: break
+        val input = readlnOrNull()?.trim() ?: break
         val parts = input.split(" ")
         when (parts[0]) {
             "Info" -> {
-                println("Engine: ${if (car.isEngineOn()) "on" else "off"}")
-                println("Direction: ${car.getDirection()}")
-                println("Speed: ${car.getSpeed()}")
-                println("Gear: ${car.getGear()}")
+                println("Engine: ${if (car.IsTurnedOn()) "on" else "off"}")
+                println("Direction: ${car.GetDirection()}")
+                println("Speed: ${car.GetSpeed()}")
+                println("Gear: ${car.GetGear()}")
             }
+
             "EngineOn" -> {
-                if (car.turnOnEngine()) println("Engine is turned on.")
-                else println("Engine is already turned on.")
+                if (car.TurnOnEngine()) {
+                    println("Engine is turned on.")
+                } else {
+                    println("Engine is already turned on.")
+                }
             }
+
             "EngineOff" -> {
-                if (car.turnOffEngine()) println("Engine is turned off.")
+                if (car.TurnOffEngine()) println("Engine is turned off.")
                 else println("Engine can't be turned off at the moment.")
             }
+
             "SetGear" -> {
                 val gear = parts[1].toIntOrNull()
                 if (gear != null) {
-                    if (car.setGear(gear)) println("Gear set to $gear.")
+                    if (car.SetGear(gear)) println("Gear set to $gear.")
                     else println("Can't set gear to $gear.")
                 } else {
                     println("Invalid input for gear.")
                 }
             }
+
             "SetSpeed" -> {
                 val speed = parts[1].toIntOrNull()
                 if (speed != null) {
-                    if (car.setSpeed(speed)) println("Speed set to $speed.")
-                    else println("Can't set speed to $speed.")
+                    if (car.SetSpeed(speed)) {
+                        println("Speed set to $speed.")
+                    } else {
+                        println("Can't set speed to $speed.")
+                    }
                 } else {
                     println("Invalid input for speed.")
                 }
             }
+
             else -> println("Unknown command")
         }
     }
--- tasks/03/1_car/car/src/test/kotlin/CarTest.kt
+++ tasks/03/1_car/car/src/test/kotlin/CarTest.kt
@@ -11,82 +11,83 @@ class CarTest {
     }
 
     @Test
-    fun testIsEngineOn() {
-        assertFalse(car.isEngineOn())
-        car.turnOnEngine()
-        assertTrue(car.isEngineOn())
+    fun testIsTurnedOn() {
+        car.TurnOffEngine()
+        assertFalse(car.IsTurnedOn())
+        car.TurnOnEngine()
+        assertTrue(car.IsTurnedOn())
     }
 
     @Test
     fun testGetDirection() {
-        assertEquals(Direction.STANDING_STILL, car.getDirection())
-        car.setGear(-1)
-        assertEquals(Direction.BACKWARD, car.getDirection())
-        car.setSpeed(10)
-        assertEquals(Direction.BACKWARD, car.getDirection())
-        car.setSpeed(0)
-        assertEquals(Direction.STANDING_STILL, car.getDirection())
-        car.setGear(1)
-        assertEquals(Direction.FORWARD, car.getDirection())
+        assertEquals(Direction.STANDING_STILL, car.GetDirection())
+        car.SetGear(-1)
+        assertEquals(Direction.BACKWARD, car.GetDirection())
+        car.SetSpeed(10)
+        assertEquals(Direction.BACKWARD, car.GetDirection())
+        car.SetSpeed(0)
+        assertEquals(Direction.STANDING_STILL, car.GetDirection())
+        car.SetGear(1)
+        assertEquals(Direction.FORWARD, car.GetDirection())
     }
 
     @Test
     fun testGetSpeed() {
-        assertEquals(0, car.getSpeed())
-        car.setSpeed(50)
-        assertEquals(50, car.getSpeed())
+        assertEquals(0, car.GetSpeed())
+        car.SetSpeed(50)
+        assertEquals(50, car.GetSpeed())
     }
 
     @Test
     fun testGetGear() {
-        assertEquals(0, car.getGear())
-        car.setGear(2)
-        assertEquals(2, car.getGear())
+        assertEquals(0, car.GetGear())
+        car.SetGear(2)
+        assertEquals(2, car.GetGear())
     }
 
     @Test
     fun testTurnOnEngine() {
-        assertFalse(car.isEngineOn())
-        assertTrue(car.turnOnEngine())
-        assertTrue(car.isEngineOn())
+        assertFalse(car.IsTurnedOn())
+        assertTrue(car.TurnOnEngine())
+        assertTrue(car.IsTurnedOn())
     }
 
     @Test
     fun testTurnOffEngine() {
-        assertFalse(car.turnOffEngine()) // Engine is already off
-        car.turnOnEngine()
-        assertFalse(car.turnOffEngine()) // Can't turn off engine while moving
-        car.setGear(1)
-        car.setSpeed(20)
-        assertFalse(car.turnOffEngine()) // Can't turn off engine while moving
-        car.setSpeed(0)
-        assertTrue(car.turnOffEngine()) // Engine turned off
-        assertFalse(car.isEngineOn())
+        assertFalse(car.TurnOffEngine()) // Engine is already off
+        car.TurnOnEngine()
+        assertFalse(car.TurnOffEngine()) // Can't turn off engine while moving
+        car.SetGear(1)
+        car.SetSpeed(20)
+        assertFalse(car.TurnOffEngine()) // Can't turn off engine while moving
+        car.SetSpeed(0)
+        assertTrue(car.TurnOffEngine()) // Engine turned off
+        assertFalse(car.IsTurnedOn())
     }
 
     @Test
     fun testSetGear() {
-        assertTrue(car.setGear(2)) // Can set any gear when engine is off
-        assertFalse(car.setGear(1)) // Can't set gear without turning on engine
-        car.turnOnEngine()
-        assertTrue(car.setGear(1)) // Can set gear after turning on engine
-        assertFalse(car.setGear(10)) // Invalid gear
-        assertTrue(car.setGear(-1)) // Can set reverse gear
-        assertTrue(car.setGear(0)) // Can set neutral gear
-        car.setSpeed(20)
-        assertFalse(car.setGear(4)) // Can't set gear while moving
-        assertTrue(car.setGear(0)) // Can set gear when speed is 0
+        assertTrue(car.SetGear(2)) // Can set any gear when engine is off
+        assertFalse(car.SetGear(1)) // Can't set gear without turning on engine
+        car.TurnOnEngine()
+        assertTrue(car.SetGear(1)) // Can set gear after turning on engine
+        assertFalse(car.SetGear(10)) // Invalid gear
+        assertTrue(car.SetGear(-1)) // Can set reverse gear
+        assertTrue(car.SetGear(0)) // Can set neutral gear
+        car.SetSpeed(20)
+        assertFalse(car.SetGear(4)) // Can't set gear while moving
+        assertTrue(car.SetGear(0)) // Can set gear when speed is 0
     }
 
     @Test
     fun testSetSpeed() {
-        assertFalse(car.setSpeed(30)) // Can't set speed without turning on engine
-        car.turnOnEngine()
-        assertTrue(car.setSpeed(30)) // Can set speed after turning on engine
-        car.setGear(1)
-        assertFalse(car.setSpeed(40)) // Can't exceed speed limit for gear
-        assertTrue(car.setSpeed(20)) // Can set valid speed
-        car.setGear(0)
-        assertFalse(car.setSpeed(10)) // Can't accelerate on neutral gear
+        assertFalse(car.SetSpeed(30)) // Can't set speed without turning on engine
+        car.TurnOnEngine()
+        assertTrue(car.SetSpeed(30)) // Can set speed after turning on engine
+        car.SetGear(1)
+        assertFalse(car.SetSpeed(40)) // Can't exceed speed limit for gear
+        assertTrue(car.SetSpeed(20)) // Can set valid speed
+        car.SetGear(0)
+        assertFalse(car.SetSpeed(10)) // Can't accelerate on neutral gear
     }
 }
