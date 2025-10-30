0abbb15195958b01c9dd2e3fb1b4c73e5d9703fb
alexander <platov0102@gmail.com>
2025-10-30 16:04:08 +0300

    2-3-2 Вынес обработку выхода в отдельный файл

--- tasksKotlin/src/main/kotlin/lab_2/dictionary_2_3_2/MainDictionary.kt
+++ tasksKotlin/src/main/kotlin/lab_2/dictionary_2_3_2/MainDictionary.kt
@@ -1,7 +1,6 @@
 package lab_2.dictionary_2_3_2
 
 import java.io.File
-import java.util.*
 
 fun main(args: Array<String>) {
     if (args.isEmpty()) {
@@ -40,21 +39,5 @@ fun main(args: Array<String>) {
         }
     }
 
-    if (dictionary.hasModifications()) {
-        print("В словарь были внесены изменения. Введите Y или y для сохранения перед выходом.\n>")
-
-        val saveChoice = readlnOrNull()?.trim()?.lowercase(Locale.getDefault())
-
-        if (saveChoice == "y") {
-            try {
-                dictionary.saveDictionary()
-
-                print("Изменения сохранены. До свидания.")
-            } catch (e: Exception) {
-                println("Ошибка при сохранении словаря: ${e.message}")
-            }
-        } else {
-            print("Изменения не сохранены. До свидания.")
-        }
-    }
+    handleExit(dictionary)
 }
